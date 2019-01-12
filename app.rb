7b8d4e853cd4770ef6d715d1ca1fb6939eab01cd
OSSahota <osstechunlimited@gmail.com>
2019-01-12 21:23:32 +0000

    First feature test - posting a peep is now green.

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/peep_new' do
+    erb :peep_new
+  end
+
+  post '/peep_post' do
+    erb :peep_post
+  end
 end
--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -2,11 +2,11 @@ feature 'Posting a peep' do
   scenario 'User posts a new peep' do
     visit '/'
     click_on 'New Peep'
-    visit '/new'
-    fill_in 'peep', with: 'First peep'
-    fill_in 'user', with: 'User A'
+    visit '/peep_new'
+    fill_in 'Peep', with: 'First peep'
+    fill_in 'User', with: 'User A'
     click_on 'Post Peep'
-    expect(current_path).to eq '/'
+    expect(current_path).to eq '/peep_post'
     expect(page).to have_content('First peep')
  end
 end
